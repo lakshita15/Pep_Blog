87e66d9918d045b9017a672355ba540442716fa2
lakshita15 <lakshitamahajan2001@gmail.com>
2021-08-12 06:02:23 +0530

    ✔

--- script.js
+++ script.js
@@ -21,4 +21,14 @@ $(document).ready(function(){
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
+    $('.header-right a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 })
